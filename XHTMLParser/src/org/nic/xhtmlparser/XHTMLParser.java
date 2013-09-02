414ab98ef9eaca9848cbb65a2ac6d9de7e6a55a7
nballmann <Administrator@10.0.2.15>
2013-09-02 11:34:38 +0200

    com02092013

--- XHTMLParser/src/org/nic/xhtmlparser/XHTMLParser.java
+++ XHTMLParser/src/org/nic/xhtmlparser/XHTMLParser.java
@@ -1,3 +1,29 @@
+/*-**********************************************************************************************
+ * 																								*
+ * Copyright (c) 2013, N.Ballmann 																*
+ * All rights reserved.																			*
+ *																								*
+ * Redistribution and use in source and binary forms, with or without modification, 			*
+ * are permitted provided that the following conditions are met:								*
+ * 																								*
+ * Redistributions of source code must retain the above copyright notice, this list of 			*
+ * conditions and the following disclaimer. 													*
+ * Redistributions in binary form must reproduce the above copyright notice, this list 			*
+ * of conditions and the following disclaimer in the documentation and/or other materials 		*
+ * provided with the distribution.																*
+ * 																								*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 			*
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 		*
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 			*
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 	*
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 	*
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 	*
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 	*
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, *
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.											*
+ * 																								*
+ ************************************************************************************************/	
+
 package org.nic.xhtmlparser;
 
 import java.io.IOException;
@@ -24,11 +50,24 @@ import org.nic.xhtmlparser.model.NewsFeed;
 import org.nic.xhtmlparser.util.ConnectionHelper;
 import org.nic.xhtmlparser.util.NewsParser;
 
+/**
+ * 
+ * This class extends {@link Application} and acts as the base entry point 
+ * for the whole JavaFX 2.2 Application
+ * 
+ * @author N.Ballmann
+ *
+ */
 public class XHTMLParser extends Application {
 	
 	private static final String APP_VERSION = "0.1A";
 	private static final String APP_TITLE = "Diabetes App Ver: " + APP_VERSION;
 	
+	/*
+	 * 
+	 * Panel Controller
+	 * 
+	 */
 	private MainViewController mainViewController;
 	private NewsFeedPaneController newsFeedController;
 	private MainMenuController mainMenuController;
@@ -80,6 +119,14 @@ public class XHTMLParser extends Application {
 		
 	}
 	
+	/**
+	 * 
+	 * initializes the Main Menu and loads the corresponding controller
+	 * {@link MainMenuController}
+	 * adds the controller to the controller list
+	 * and sets the pane as first item to show
+	 * 
+	 */
 	public void initMainMenuPane() {
 		
 		try {
@@ -95,12 +142,22 @@ public class XHTMLParser extends Application {
 			mainViewController.addScreen("MainMenu", fPane);
 			mainViewController.setScreen(fPane);
 			
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			
 		}
 		
 	}
 	
+	/**
+	 * 
+	 * initializes the News Pane and loads the corresponding controller
+	 * {@link NewsFeedController}
+	 * adds the controller to the controller list
+	 * and populates the News Feeds via background parsing 
+	 * 
+	 */
 	public void initNewsFeedPane() {
 		
 		try {
@@ -140,6 +197,13 @@ public class XHTMLParser extends Application {
 		
 	}
 	
+	/**
+	 * 
+	 * initializes the Calendar Pane and loads the corresponding controller
+	 * {@link CalendarControllerr}
+	 * adds the controller to the controller list
+	 * 
+	 */
 	public void initNewEntryPane() {
 		
 		try {
@@ -172,6 +236,15 @@ public class XHTMLParser extends Application {
 		
 	}
 	
+	/**
+	 * nested class NewsFeedService extends {@link Service}
+	 * 
+	 * for background parsing and populating of the NewsFeedPane
+	 * allows for restricting access to the corresponding pane 
+	 * until the {@link EventHandler}<{@link WorkerStateEvent}> 
+	 * fires and the Task state transitions to the SUCCEEDED state.
+	 * 
+	 */
 	private static class NewsFeedService extends Service<ArrayList<NewsFeed>> {
 
 		@Override
